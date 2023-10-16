5b74f1c71ac69e18cc5ed57ce2c93a4ee8678446
taniyasharma12 <85053701+taniyasharma12@users.noreply.github.com>
2023-10-16 15:35:57 +0530

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,15 @@
+
+public class StringReverse {
+
+	public static void main (String [] args) {
+		String name = "Hello World";
+		String rev = "";
+		
+		for(int i=name.length()-1; i>=0; i--) {
+			
+			rev = rev + name.charAt(i);
+			
+		}
+		System.out.println(rev);
+	}
+}
