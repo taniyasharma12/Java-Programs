b573f91d2da8c407efce09c354ed7dd284f87833
taniyasharma12 <85053701+taniyasharma12@users.noreply.github.com>
2024-09-10 19:55:28 +0530

    Update MinimumSizeSubarraySumLeetcode.java

--- Sliding Window/MinimumSizeSubarraySumLeetcode.java
+++ Sliding Window/MinimumSizeSubarraySumLeetcode.java
@@ -1,54 +1,67 @@
-package SlidingWindowFixedSize;
+package SlidingWindowVariableSize;
 
 public class MinimumSizeSubarraySumLeetcode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int nums [] = {1,4,4};
+		int nums [] = {1,1,4,4};
 		
-		int target = 1;
+		int target = 5;
 		
 		System.out.println(minSubArrayLen(target,nums));
+		
+		
 		
 //https://leetcode.com/problems/minimum-size-subarray-sum/description/?envType=study-plan-v2&envId=top-interview-150
 	}
 	
+	/*
+	 1. Take two pointers i and j, take variable sum to store sum of elements, 
+	    take minLength to store minimum length
+	 2. while j< array length add sum and do j++
+	 4. if cum==target , assign minLength
+	 5. if sum>target remove ith element and do i++, if sum==target calculate minLength
+	 5. increment j++
+	 6. return minLength 	 
+	 */
 	 public static int minSubArrayLen(int target, int[] nums) {
 		
-/*
- 1. Take two pointers i and j, take variable currSum to store sum of elements, take minWindowSize to store minimum length
- 2. while j< array length add sum and do j++
- 4. if sum >= target , take minimum length from j-i+1 and minLen
- 5. decrease sum of ith element from sum and increment i++
- 6. return minLength 	 
- */
 		 int i=0;
 		 int j=0;
-		 int currSum=0;
-		 int minWindowSize=Integer.MAX_VALUE;
-	
-		 while(j<nums.length) {
-			 
-			 currSum+=nums[j];
-			 
-				 j++;
-			 
-			while(currSum>=target) {
-				 
-				 minWindowSize = Math.min(j-i, minWindowSize);
-				 
-				 currSum-=nums[i];
-				 i++;
-			 }
-		 }
-		 if(minWindowSize==Integer.MAX_VALUE){
-			    return 0;
-		 }
-			else
-			{
-			return minWindowSize;
-			    }
+		 int minLength = Integer.MAX_VALUE;
+		 int sum=0;
+		 
+		while(j<nums.length) {
+			
+			sum+=nums[j];
+			
+			if(sum<target) {
+				
+				j++;
 			}
+			else if(sum==target) {
+				
+				minLength = Math.min(minLength, j-i+1);
+				
+				j++;
+			}
+			else {
+				
+				while(sum>target) {
+					
+					sum-=nums[i];
+					i++;
+				
+				if(sum==target) {
+					
+					minLength = Math.min(minLength, j-i+1);	
+				}
+			}
+			j++;
+		}
+		}	 
+		 return minLength;
 
+	 }
 }
