a8b786e9f13fd287ab1de5c826655ff74a76ec75
taniyasharma12 <85053701+taniyasharma12@users.noreply.github.com>
2024-09-21 16:26:53 +0530

    Add files via upload

---
+++ BasicJavaPrograms/CheckIfNIsPerfectPowerOf3.java
@@ -0,0 +1,11 @@
+package BasicJavaPrograms;
+
+public class CheckIfNIsPerfectPowerOf3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
---
+++ BasicJavaPrograms/FindPowAtoN.java
@@ -0,0 +1,10 @@
+package BasicJavaPrograms;
+
+public class FindPowAtoN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
